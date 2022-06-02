74541d6e4fc08a1bf49ef600d95ed598d16ab3c3
n.shchukin <n.shchukin@yclients.tech>
2022-06-02 23:32:47 +0300

    feat(project)

---
+++ internal/database/migrations/2_create_orders_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE orders
+(
+    id          SERIAL PRIMARY KEY,
+    user_id     INTEGER,
+    number      VARCHAR(255) UNIQUE,
+    status      VARCHAR(255),
+    accrual     INTEGER,
+    uploaded_at TIMESTAMP DEFAULT NOW()
+); 
